bbc3f54fa5bbd0f0007f7bf2a7e70eb15dcb8098
msegal347 <msegal0347@gmail.com>
2023-10-16 20:51:47 -0400

    updated analyticsService.test

--- test/services/analyticsService.test.ts
+++ test/services/analyticsService.test.ts
@@ -1,16 +1,28 @@
-// Import only for type information and mocking
 import * as AnalyticsService from '../../src/services/analyticsService';
+import AnalyticsModel from '../../src/models/analyticsModel';
+
+// Mock the AnalyticsModel class
+jest.mock('../../src/models/analyticsModel');
 
 describe('AnalyticsService', () => {
   it('should call AnalyticsService.createRecord and return the result', async () => {
-    // Mock data
-    const mockData = 'data';
-    const mockResult = ['data'];
+    // Mock data and result
+    const mockData = { data: 'data' };
+    const mockResult = { data: 'data', save: jest.fn() };
 
-    // Perform test
-    const result = await AnalyticsService.createRecord({ data: mockData });
+    // Mock the save method to resolve the promise with mockResult
+    mockResult.save.mockResolvedValueOnce(mockResult);
+
+    // Mock the AnalyticsModel constructor to return an instance with a mock save method
+    (AnalyticsModel as jest.MockedClass<typeof AnalyticsModel>).mockImplementation(() => {
+      return mockResult as any;
+    });
+
+    // Perform the test
+    const result = await AnalyticsService.createRecord(mockData);
 
     // Assertions
     expect(result).toEqual(mockResult);
   });
 });
+
