f8923d232c3a13fb11c3a6be2a7a5e3b903240ed
Godwill Agbehonou <godwill.personal@gmail.com>
2023-10-19 00:09:44 -0400

    Redoing Jest Compilation

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+  //globalSetup: './config/setup.js',
+
+  //globalTeardown: './config/teardown.js',
+
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+    'node_modules/axios': 'babel-jest',
+  },
+  
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec|fuzz))\\.(jsx?|tsx?)$',
+  
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/main.ts', 
+  ],
+  
+  testEnvironment: 'node',
+
+  transformIgnorePatterns: ['<rootDir>/node_modules/', 'node_modules/(?!(axios)/)'],
+};
