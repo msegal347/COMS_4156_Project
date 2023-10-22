43520a19735c93c922befb5ce6e30cb90f4be2bb
Justin Hudis <justin.hudis@gmail.com>
2023-10-22 19:48:48 +0000

    Attempt to get database tests working

--- test/database/database.test.ts
+++ test/database/database.test.ts
@@ -1,17 +1,28 @@
+import mongoose from 'mongoose';
 import EntityService from '../../src/services/entityService';
 import { resourceService } from '../../src/services/resourceService';
 import { transactionService } from '../../src/services/transactionService';
 import { getRouteById } from '../../src/services/logisticsService';
 import { notificationService } from '../../src/services/notificationService';
 import { getRecordById } from '../../src/services/analyticsService';
+// import connectDB from '../../src/config/db';
+
+// beforeEach(() => {
+  // connectDB();
+  // jest.useFakeTimers();
+// });
 
 describe('MongoDB', () => {
   it('should have the correct seed data', () => {
     expect(EntityService.getEntityById('test_entity')).toBe({ entityID: 'test_entity', role: 'source', apiKey: 'key', coordinates: { latitude: 0.5, longitude: 0.5 } });
-    expect(resourceService.getResourceById('test_resource')).toBe({ resourceID: 'test_resource', metadata: { type: 'type', quantity: 0, expirationDate: new Date() } });
+    expect(resourceService.getResourceById(new mongoose.Types.ObjectId('test_resource'))).toBe({ resourceID: 'test_resource', metadata: { type: 'type', quantity: 0, expirationDate: new Date() } });
     expect(transactionService.getTransactionById('test_transaction')).toBe({ transactionID: 'test_transaction', sourceID: 'test_entity', sinkID: 'test_entity', resourceID: 'test_resource', timestamp: new Date() });
     expect(getRouteById('test_logistics')).toBe({ logisticsID: 'test_logistics', pickupLocation: { latitude: 0.5, longitude: 0.5 }, dropOffLocation: { latitude: 0.5, longitude: 0.5 }, status: 'scheduled' });
-    expect(notificationService.getNotifications(null)[0]).toBe({ notificationID: 'test_notification', type: 'type', status: 'sent' });
+    expect(notificationService.getNotifications(new mongoose.Schema.Types.ObjectId('test_notification'))[0]).toBe({ notificationID: 'test_notification', type: 'type', status: 'sent' });
     expect(getRecordById('test_analytics')).toBe({ analyticsID: 'test_analytics', data: {} });
   });
 });
+
+// afterAll(() => {
+//   mongoose.connection.close();
+// });
