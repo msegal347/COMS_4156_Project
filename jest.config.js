db8b4350781796b962c9ecd3284b7fe2091a19ab
Godwill Agbehonou <godwill.personal@gmail.com>
2023-10-24 22:18:04 -0400

    Updated Jest to work with Fuzzing

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,57 @@
 module.exports = {
-
-  //globalSetup: './config/setup.js',
-
-  //globalTeardown: './config/teardown.js',
-
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-  
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.jsx?$': 'babel-jest',
-    'node_modules/axios': 'babel-jest',
-  },
-  
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  
   collectCoverage: true,
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/main.ts', 
   ],
-  
-  testEnvironment: 'node',
+  verbose: true,
+	projects: [
+		{
+			displayName: "Jest",
+			preset: "ts-jest",
+      //globalSetup: './config/setup.js',
 
-  transformIgnorePatterns: ['<rootDir>/node_modules/', 'node_modules/(?!(axios)/)'],
+      //globalTeardown: './config/teardown.js',
 
-  "bail": 1,
+      roots: ['<rootDir>/src', '<rootDir>/test'],
+      
+      transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.jsx?$': 'babel-jest',
+        'node_modules/axios': 'babel-jest',
+      },
+      
+      testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+      
+      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
-  "verbose": false
+      extensionsToTreatAsEsm: ['.ts', '.tsx'],
+      
+      testEnvironment: 'node',
+
+      transformIgnorePatterns: ['<rootDir>/node_modules/', 'node_modules/(?!(axios)/)'],
+
+      
+		},
+		{
+			displayName: {
+				name: "Jazzer.js",
+				color: "cyan",
+			},
+      preset: "ts-jest",
+      testRunner: "@jazzer.js/jest-runner",
+			testEnvironment: "node",
+      ////testRegex: '(/__tests__/.*|(\\.|/)(fuzz))\\.(jsx?|tsx?)$',
+      //testMatch: ["<rootDir>/**/*.fuzz.[jt]s"]
+      testMatch: ["<rootDir>/docker/fuzz/math.fuzz.ts"],
+      transform: {
+        '^.+\\.fuzz.(ts|tsx)$': 'ts-jest',
+      },
+			
+		},
+	],
+	coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+	modulePathIgnorePatterns: ["/node_modules", "/dist/"],
 
 };
