a3f5df37353858ff66ec02605fe344d1db738b3e
Justin Hudis <justin.hudis@gmail.com>
2023-10-25 12:27:12 -0400

    Add more descriptive HTTP request logging

--- dist/config/logger.js
+++ dist/config/logger.js
@@ -1,27 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logger = void 0;
-const winston_1 = require("winston");
-// Define custom format
-const { combine, timestamp, label, printf } = winston_1.format;
-const customFormat = printf(({ level, message, label, timestamp }) => {
-    return `[${timestamp}] [${label}] ${level}: ${message}`;
-});
-// Create a logger instance
-const winstonLogger = (0, winston_1.createLogger)({
-    format: combine(label({ label: 'FoodLink-API' }), timestamp(), customFormat),
-    transports: [new winston_1.transports.Console(), new winston_1.transports.File({ filename: 'combined.log' })],
-});
-// Logger has request, response, and next function as parameters
-const logger = (req, res, next) => {
-    if (typeof req.method !== 'string' || typeof req.url !== 'string') {
-        winstonLogger.error('Invalid request object');
-        next(new Error('Invalid request object'));
-        return;
-    }
-    winstonLogger.info(`HTTP ${req.method} ${req.url}`);
-    next();
-};
-exports.logger = logger;
+const morgan_1 = __importDefault(require("morgan"));
+exports.logger = (0, morgan_1.default)('combined');
 exports.default = exports.logger;
 //# sourceMappingURL=logger.js.map
