00f7b819b9adc30d78e8d8a677cdea2687d31ecc
Justin Hudis <justin.hudis@gmail.com>
2023-11-08 01:48:21 +0000

    Second attempt at passing tests

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,47 +3,45 @@ import morganJson from 'morgan-json';
 import { Client } from '@elastic/elasticsearch';
 import fs from 'fs';
 
-let ca: Buffer | undefined;
-try {
-  ca = fs.readFileSync('/usr/share/elasticsearch/config/certs/ca/ca.crt');
-} catch {
-  ca = undefined;
-}
-
-const esClient = new Client({
-  node: 'https://es01:9200',
-  auth: {
-    username: 'elastic',
-    password: process.env.ELASTIC_PASSWORD || 'changeme',
-  },
-  tls: {
-    ca: ca,
-    rejectUnauthorized: false,
-  },
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-});
-
 export const consoleLogger = morgan('combined');
 
-export const esLogger = morgan(
-  morganJson({
-    method: ':method',
-    url: ':url',
-    status: ':status',
-    responseTime: ':response-time',
-    length: ':res[content-length]',
-  }),
-  {
-    stream: {
-      write: (message: string) => {
-        esClient.index({
-          index: 'logs',
-          body: JSON.parse(message),
-        });
-      },
+export let esLogger;
+
+try {
+  const esClient = new Client({
+    node: 'https://es01:9200',
+    auth: {
+      username: 'elastic',
+      password: process.env.ELASTIC_PASSWORD || 'changeme',
     },
-  }
-);
+    tls: {
+      ca: fs.readFileSync('/usr/share/elasticsearch/config/certs/ca/ca.crt'),
+      rejectUnauthorized: false,
+    },
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  esLogger = morgan(
+    morganJson({
+      method: ':method',
+      url: ':url',
+      status: ':status',
+      responseTime: ':response-time',
+      length: ':res[content-length]',
+    }),
+    {
+      stream: {
+        write: (message: string) => {
+          esClient.index({
+            index: 'logs',
+            body: JSON.parse(message),
+          });
+        },
+      },
+    }
+  );
+} catch {
+  esLogger = undefined;
+}
