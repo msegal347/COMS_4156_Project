6e938552902e9a37e1033d20bceaa9d7c41d829a
Justin Hudis <justin.hudis@gmail.com>
2023-11-08 01:40:09 +0000

    Try/catch cert read so tests pass

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,6 +3,13 @@ import morganJson from 'morgan-json';
 import { Client } from '@elastic/elasticsearch';
 import fs from 'fs';
 
+let ca: Buffer | undefined;
+try {
+  ca = fs.readFileSync('/usr/share/elasticsearch/config/certs/ca/ca.crt');
+} catch {
+  ca = undefined;
+}
+
 const esClient = new Client({
   node: 'https://es01:9200',
   auth: {
@@ -10,7 +17,7 @@ const esClient = new Client({
     password: process.env.ELASTIC_PASSWORD || 'changeme',
   },
   tls: {
-    ca: fs.readFileSync('/usr/share/elasticsearch/config/certs/ca/ca.crt'),
+    ca: ca,
     rejectUnauthorized: false,
   },
   headers: {
