62a750eefe8c1a0e5328c308d65c2afeb59c7bbd
msegal347 <msegal0347@gmail.com>
2023-11-25 20:23:46 -0500

    updated admin page

--- frontend/foodlink-frontend/src/pages/AdminPage.js
+++ frontend/foodlink-frontend/src/pages/AdminPage.js
@@ -5,14 +5,124 @@ import axios from 'axios';
 const CollapsibleComponent = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={styles.collapsibleComponent}>
-      <button className={styles.collapsibleTitle} onClick={() => setIsOpen(!isOpen)}>
+      <button
+        className={`${styles.collapsibleTitle} ${isOpen ? styles.active : ''}`}
+        onClick={toggleCollapse}
+      >
         {title}
       </button>
-      {isOpen && <div className={styles.collapsibleContent}>{children}</div>}
+      <div className={`${styles.collapsibleContent} ${isOpen ? styles.active : ''}`}>
+        {children}
+      </div>
     </div>
   );
+};
+
+
+const auditLogsData = [
+  { id: 1, timestamp: new Date().toISOString(), event: 'User JohnD logged in', category: 'User Actions' },
+  { id: 2, timestamp: new Date().toISOString(), event: 'Backup completed successfully', category: 'System Events' },
+  { id: 3, timestamp: new Date().toISOString(), event: 'Failed login attempt for user JaneD', category: 'Security Events' },
+  { id: 4, timestamp: new Date().toISOString(), event: 'Updated system settings by admin', category: 'Administrative Actions' },
+  // ... More log entries
+];
+
+const placeholderResources = [
+  { id: 'r1', name: 'Apples', availableQuantity: 200 },
+  { id: 'r2', name: 'Oranges', availableQuantity: 150 },
+  { id: 'r3', name: 'Bananas', availableQuantity: 180 },
+  { id: 'r4', name: 'Grapes', availableQuantity: 210 },
+  { id: 'r5', name: 'Peaches', availableQuantity: 170 },
+];
+
+const placeholderTransactions = [
+  {
+    id: 't1',
+    materials: [{ foodType: 'Apples', quantity: 100 }],
+    origin: 'Central Park, NY',
+    destination: 'Times Square, NY',
+    orderDate: new Date('2023-11-25T09:00:00Z'),
+    expectedDelivery: new Date('2023-11-26T09:00:00Z'),
+    start: { lat: 40.7812, lng: -73.9665 },
+    end: { lat: 40.7580, lng: -73.9855 },
+  },
+  {
+    id: 't2',
+    materials: [{ foodType: 'Oranges', quantity: 150 }],
+    origin: 'Empire State Building, NY',
+    destination: 'Statue of Liberty, NY',
+    orderDate: new Date('2023-11-25T10:00:00Z'),
+    expectedDelivery: new Date('2023-11-26T10:00:00Z'),
+    start: { lat: 40.748817, lng: -73.985428 },
+    end: { lat: 40.689247, lng: -74.044502 },
+  },
+  {
+    id: 't3',
+    materials: [{ foodType: 'Bananas', quantity: 120 }],
+    origin: 'Brooklyn Bridge, NY',
+    destination: 'Wall Street, NY',
+    orderDate: new Date('2023-11-25T11:00:00Z'),
+    expectedDelivery: new Date('2023-11-26T11:00:00Z'),
+    start: { lat: 40.7061, lng: -73.9969 },
+    end: { lat: 40.7074, lng: -74.0113 },
+  },
+  {
+    id: 't4',
+    materials: [{ foodType: 'Grapes', quantity: 90 }],
+    origin: 'Madison Square Garden, NY',
+    destination: 'Yankee Stadium, NY',
+    orderDate: new Date('2023-11-25T12:00:00Z'),
+    expectedDelivery: new Date('2023-11-26T12:00:00Z'),
+    start: { lat: 40.7505, lng: -73.9934 },
+    end: { lat: 40.8296, lng: -73.9262 },
+  },
+  {
+    id: 't5',
+    materials: [{ foodType: 'Peaches', quantity: 80 }],
+    origin: 'Central Park Zoo, NY',
+    destination: 'Coney Island, NY',
+    orderDate: new Date('2023-11-25T13:00:00Z'),
+    expectedDelivery: new Date('2023-11-26T13:00:00Z'),
+    start: { lat: 40.7678, lng: -73.9718 },
+    end: { lat: 40.5749, lng: -73.9857 },
+  },
+];
+
+const analyticsData = {
+  userActivity: {
+    loginsToday: 150,
+    signupsToday: 10,
+    activeUsers: 75,
+  },
+  transactionVolumes: {
+    today: 120,
+    thisWeek: 840,
+    thisMonth: 3600,
+  },
+  resourceUtilization: {
+    totalListed: 300,
+    totalRequested: 150,
+    totalFulfilled: 100,
+  },
+  systemPerformance: {
+    uptime: '99.99%',
+    averageResponseTime: '200ms',
+  },
+  issueTracking: {
+    openIssues: 5,
+    resolvedIssues: 20,
+    averageResolutionTime: '48 hours',
+  },
+  userEngagement: {
+    averageTransactionsPerUser: 5,
+    averageSessionTime: '5 minutes',
+  },
 };
 
 const AdminPage = () => {
@@ -25,8 +135,8 @@ const AdminPage = () => {
 
   // States for various sections
   const [users, setUsers] = useState(placeholderUsers);
-  const [resources, setResources] = useState([]);
-  const [transactions, setTransactions] = useState([]);
+  const [resources, setResources] = useState(placeholderResources);
+  const [transactions, setTransactions] = useState(placeholderTransactions);
   const [analytics, setAnalytics] = useState({});
   const [settings, setSettings] = useState({ sampleSetting: '' });
   const [auditLogs, setAuditLogs] = useState([]);
@@ -105,26 +215,89 @@ const AdminPage = () => {
       </CollapsibleComponent>
 
       {/* Resource Oversight Section */}
-      <section className={styles.section}>
-        <h2>Resource Oversight</h2>
-        {resources.map(resource => (
-          <div key={resource.id}>{resource.name}</div>
-        ))}
-      </section>
+      <CollapsibleComponent title="Resource Oversight">
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Available Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {resources.map((resource) => (
+              <tr key={resource.id}>
+                <td>{resource.id}</td>
+                <td>{resource.name}</td>
+                <td>{resource.availableQuantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </CollapsibleComponent>
 
       {/* Transaction Oversight Section */}
-      <section className={styles.section}>
-        <h2>Transaction Oversight</h2>
-        {transactions.map(transaction => (
-          <div key={transaction.id}>{transaction.name}</div>
-        ))}
-      </section>
+      <CollapsibleComponent title="Transaction Oversight">
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Food Type</th>
+              <th>Quantity</th>
+              <th>Origin</th>
+              <th>Destination</th>
+              <th>Order Date</th>
+              <th>Expected Delivery</th>    
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td>{transaction.id}</td>
+                <td>{transaction.materials[0].foodType}</td>
+                <td>{transaction.materials[0].quantity}</td>
+                <td>{`${transaction.start.lat.toFixed(4)}, ${transaction.start.lng.toFixed(4)}`}</td>
+                <td>{`${transaction.end.lat.toFixed(4)}, ${transaction.end.lng.toFixed(4)}`}</td>
+                <td>{transaction.orderDate.toLocaleString()}</td>
+                <td>{transaction.expectedDelivery.toLocaleString()}</td>      
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </CollapsibleComponent>
 
       {/* Analytics and Reporting Section */}
-      <section className={styles.section}>
-        <h2>Analytics and Reporting</h2>
-        <div>{JSON.stringify(analytics)}</div>
-      </section>
+      <CollapsibleComponent title="Analytics and Reporting">
+        <div className={styles.analyticsSection}>
+          <h3>User Activity</h3>
+          <p>Logins Today: {analyticsData.userActivity.loginsToday}</p>
+          <p>Signups Today: {analyticsData.userActivity.signupsToday}</p>
+          <p>Active Users: {analyticsData.userActivity.activeUsers}</p>
+          
+          <h3>Transaction Volumes</h3>
+          <p>Today: {analyticsData.transactionVolumes.today}</p>
+          <p>This Week: {analyticsData.transactionVolumes.thisWeek}</p>
+          <p>This Month: {analyticsData.transactionVolumes.thisMonth}</p>
+
+          <h3>Resource Utilization</h3>
+          <p>Total Listed: {analyticsData.resourceUtilization.totalListed}</p>
+          <p>Total Requested: {analyticsData.resourceUtilization.totalRequested}</p>
+          <p>Total Fulfilled: {analyticsData.resourceUtilization.totalFulfilled}</p>
+
+          <h3>System Performance</h3>
+          <p>Uptime: {analyticsData.systemPerformance.uptime}</p>
+          <p>Average Response Time: {analyticsData.systemPerformance.averageResponseTime}</p>
+
+          <h3>Issue Tracking</h3>
+          <p>Open Issues: {analyticsData.issueTracking.openIssues}</p>
+          <p>Resolved Issues: {analyticsData.issueTracking.resolvedIssues}</p>
+          <p>Average Resolution Time: {analyticsData.issueTracking.averageResolutionTime}</p>
+
+          <h3>User Engagement</h3>
+          <p>Average Transactions Per User: {analyticsData.userEngagement.averageTransactionsPerUser}</p>
+          <p>Average Session Time: {analyticsData.userEngagement.averageSessionTime}</p>
+        </div>
+      </CollapsibleComponent>
 
       {/* System Settings Section */}
       <section className={styles.section}>
@@ -145,12 +318,28 @@ const AdminPage = () => {
       </section>
 
       {/* Audit Logs Section */}
-      <section className={styles.section}>
-        <h2>Audit Logs</h2>
-        {auditLogs.map(log => (
-          <div key={log.id}>{log.description}</div>
-        ))}
-      </section>
+      <CollapsibleComponent title="Audit Logs">
+        <table className={styles.auditLogsTable}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Timestamp</th>
+              <th>Event</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {auditLogsData.map(log => (
+              <tr key={log.id}>
+                <td>{log.id}</td>
+                <td>{log.timestamp}</td>
+                <td>{log.event}</td>
+                <td>{log.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </CollapsibleComponent>
 
       {/* Support and Issue Management Section */}
       <section className={styles.section}>
