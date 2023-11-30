5938b8a8a9a2fbb6a100ac8f9ff987e69ae6d164
msegal347 <msegal0347@gmail.com>
2023-11-30 13:33:06 -0500

    updated user service with geocoding

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,10 @@
 import User, { IUser } from '../models/userModel';
 import bcrypt from 'bcrypt';
 import { getCoordinates } from '../utils/googleMaps';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 class UserService {
   async register(email: string, password: string, role: string, address?: string) {
@@ -14,9 +18,14 @@ class UserService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    let coordinates: { latitude: number; longitude: number } | undefined;
+    let coordinates: { lat: number; lng: number } | undefined;
     if ((role === 'source' || role === 'sink') && address) {
-      coordinates = await getCoordinates(address);
+      const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+      if (!apiKey) {
+        throw new Error('Google Maps API key is not set');
+      }
+      const geocodedCoordinates = await getCoordinates(address, apiKey);
+      coordinates = { lat: geocodedCoordinates.latitude, lng: geocodedCoordinates.longitude };
     }
 
     const user = new User({ email, password: hashedPassword, role, address, coordinates });
