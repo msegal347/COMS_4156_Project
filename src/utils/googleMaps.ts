42cf2b67c4a6a03ad773893d3c9ee6b401675e23
msegal347 <msegal0347@gmail.com>
2023-11-30 10:46:03 -0500

    updated google maps util

--- src/utils/googleMaps.ts
+++ src/utils/googleMaps.ts
@@ -31,3 +31,40 @@ export const getCoordinates = async (
     throw new Error('An error occurred during the geocoding process');
   }
 };
+
+
+export const getOptimalRoute = async (
+  origin: string,
+  destinations: string[],
+  apiKey: string
+): Promise<string[]> => {
+  try {
+    console.log(`Fetching optimal route from ${origin} to`, destinations);
+
+    const originStr = `origin=${encodeURIComponent(origin)}`;
+    const destinationStr = `destination=${encodeURIComponent(destinations.pop() || '')}`;
+    const waypointsStr = `waypoints=optimize:true|${destinations
+      .map(encodeURIComponent)
+      .join('|')}`;
+
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/directions/json?${originStr}&${destinationStr}&${waypointsStr}&key=${apiKey}`
+    );
+
+    console.log(`Received response:`, response.data);
+
+    if (response.data.status === 'OK') {
+      const route = response.data.routes[0].waypoint_order.map(
+        (index: number) => destinations[index]
+      );
+      console.log(`Optimal route found:`, route);
+      return route;
+    } else {
+      console.error(`API Error: ${response.data.error_message}`);
+      throw new Error(response.data.error_message || 'Failed to get the optimal route');
+    }
+  } catch (error) {
+    console.error('Error during route optimization process:', error);
+    throw new Error('An error occurred during the route optimization process');
+  }
+};
