407106816a812a673cb9dab3ab58cc87cae10c42
msegal347 <msegal0347@gmail.com>
2023-12-07 15:10:05 -0500

    updated resource service

--- src/services/resourceService.ts
+++ src/services/resourceService.ts
@@ -6,30 +6,31 @@ export const resourceService = {
     console.log("Service: Add or Update User Resource - Data Received:", data);
     const category = await ResourceCategory.findById(data.categoryId);
     console.log("Service: Found Category:", category);
-
+  
     if (!category) {
       console.log("Service: Category not found for ID:", data.categoryId);
       throw new Error('Resource category not found');
     }
-
+  
     const objectIdUserId = new mongoose.Types.ObjectId(data.userId);
     const userResourceIndex = category.userResources.findIndex(ur => ur.userId.equals(objectIdUserId));
     console.log("Service: User Resource Index:", userResourceIndex);
-
+  
     if (userResourceIndex > -1) {
-      category.userResources[userResourceIndex].quantity = data.quantity;
+      // Updating the quantity for the specific user
+      category.userResources[userResourceIndex].quantity += data.quantity; // Use += to increment the quantity
       console.log("Service: Updated existing user resource");
     } else {
+      // Adding a new user resource
       category.userResources.push({ userId: objectIdUserId, quantity: data.quantity } as any);
       console.log("Service: Added new user resource");
     }
-
+  
     const savedCategory = await category.save();
     console.log("Service: Saved Category:", savedCategory);
     return savedCategory;
   },
-
-
+  
   async getResources(): Promise<IResourceCategory[]> {
     // Retrieving all resources along with the user-specific quantities
     return await ResourceCategory.find({}).populate('userResources.userId');
