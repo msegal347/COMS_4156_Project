db0de2f6b5b9b293e350bb34ea37b83a96136158
shvam0000 <shvam0000@gmail.com>
2023-12-13 16:17:00 -0500

    admin data from backend

--- frontend/foodlink-frontend/src/pages/AdminPage.js
+++ frontend/foodlink-frontend/src/pages/AdminPage.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import styles from './AdminPage.module.css';
-import { getResources, getRecentTransactions, getUsers, getAuditLogs, getAnalytics } from '../services/api';
+import {
+  getResources,
+  getRecentTransactions,
+  getUsers,
+  getAuditLogs,
+  getAnalytics,
+} from '../services/api';
 
 const CollapsibleComponent = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,19 +31,73 @@ const CollapsibleComponent = ({ title, children }) => {
 };
 
 const placeholderUsers = [
-  { userId: "u656fe3661a0dcc3c4bcecf02", userName: "source1@example.com", role: "source", address: "123 Source St" },
-  { userId: "u656fe3661a0dcc3c4bcecf03", userName: "sink1@example.com", role: "sink", address: "456 Sink Rd" },
-  { userId: "u656fe3661a0dcc3c4bcecf04", userName: "auditor1@example.com", role: "auditor", address: "789 Auditor Ave" },
-  { userId: "u656fe3661a0dcc3c4bcecf06", userName: "source2@example.com", role: "source", address: "102 Source St" },
-  { userId: "u656fe3661a0dcc3c4bcecf07", userName: "sink2@example.com", role: "sink", address: "103 Sink Street" },
-  { userId: "u656fe3661a0dcc3c4bcecf08", userName: "auditor2@example.com", role: "auditor", address: "104 Auditor Rd" },
-  { userId: "u656fe3661a0dcc3c4bcecf0a", userName: "source3@example.com", role: "source", address: "106 Source St" },
-  { userId: "u656fe3661a0dcc3c4bcecf0b", userName: "sink3@example.com", role: "sink", address: "107 Sink Blvd" },
-  { userId: "u656fe3661a0dcc3c4bcecf0c", userName: "auditor3@example.com", role: "auditor", address: "108 Auditor St" },
-  { userId: "u6570a44b66b2b54d698fb4a2", userName: "sink_test@test.com", role: "sink", address: "535 West 116th St" },
-  { userId: "u6570a44b66b2b54d698fb4a2b", userName: "source_test@test.com", role: "source", address: "537 West 116th St" },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf02',
+    userName: 'source1@example.com',
+    role: 'source',
+    address: '123 Source St',
+  },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf03',
+    userName: 'sink1@example.com',
+    role: 'sink',
+    address: '456 Sink Rd',
+  },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf04',
+    userName: 'auditor1@example.com',
+    role: 'auditor',
+    address: '789 Auditor Ave',
+  },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf06',
+    userName: 'source2@example.com',
+    role: 'source',
+    address: '102 Source St',
+  },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf07',
+    userName: 'sink2@example.com',
+    role: 'sink',
+    address: '103 Sink Street',
+  },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf08',
+    userName: 'auditor2@example.com',
+    role: 'auditor',
+    address: '104 Auditor Rd',
+  },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf0a',
+    userName: 'source3@example.com',
+    role: 'source',
+    address: '106 Source St',
+  },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf0b',
+    userName: 'sink3@example.com',
+    role: 'sink',
+    address: '107 Sink Blvd',
+  },
+  {
+    userId: 'u656fe3661a0dcc3c4bcecf0c',
+    userName: 'auditor3@example.com',
+    role: 'auditor',
+    address: '108 Auditor St',
+  },
+  {
+    userId: 'u6570a44b66b2b54d698fb4a2',
+    userName: 'sink_test@test.com',
+    role: 'sink',
+    address: '535 West 116th St',
+  },
+  {
+    userId: 'u6570a44b66b2b54d698fb4a2b',
+    userName: 'source_test@test.com',
+    role: 'source',
+    address: '537 West 116th St',
+  },
 ];
-
 
 const auditLogsData = [
   {
@@ -94,7 +154,7 @@ const placeholderTransactions = [
     destination: 'Columbia University, NY',
     orderDate: new Date('2023-12-06T11:00:00Z'),
     expectedDelivery: new Date('2023-12-07T11:00:00Z'),
-    start: { lat: 40.7420, lng: -74.0048 },
+    start: { lat: 40.742, lng: -74.0048 },
     end: { lat: 40.807536, lng: -73.962573 },
   },
   {
@@ -105,7 +165,7 @@ const placeholderTransactions = [
     orderDate: new Date('2023-12-06T12:00:00Z'),
     expectedDelivery: new Date('2023-12-07T12:00:00Z'),
     start: { lat: 40.730823, lng: -73.997332 },
-    end: { lat: 40.811550, lng: -73.946477 },
+    end: { lat: 40.81155, lng: -73.946477 },
   },
   {
     id: 't5',
@@ -124,7 +184,7 @@ const placeholderTransactions = [
     destination: 'Financial District, NY',
     orderDate: new Date('2023-12-06T14:00:00Z'),
     expectedDelivery: new Date('2023-12-07T14:00:00Z'),
-    start: { lat: 40.7420, lng: -74.0048 },
+    start: { lat: 40.742, lng: -74.0048 },
     end: { lat: 40.707491, lng: -74.011276 },
   },
   {
@@ -154,7 +214,7 @@ const placeholderTransactions = [
     destination: 'Lower East Side, NY',
     orderDate: new Date('2023-12-06T17:00:00Z'),
     expectedDelivery: new Date('2023-12-07T17:00:00Z'),
-    start: { lat: 40.7420, lng: -74.0048 },
+    start: { lat: 40.742, lng: -74.0048 },
     end: { lat: 40.718267, lng: -73.988065 },
   },
   {
@@ -166,7 +226,7 @@ const placeholderTransactions = [
     expectedDelivery: new Date('2023-12-07T18:00:00Z'),
     start: { lat: 40.730823, lng: -73.997332 },
     end: { lat: 40.723301, lng: -74.002988 },
-  }
+  },
 ];
 
 const analyticsData = {
@@ -201,33 +261,94 @@ const analyticsData = {
 };
 
 const AdminPage = () => {
-  const [users, setUsers] = useState(placeholderUsers)
-  const [resources, setResources] = useState([]); 
-  const [transactions] = useState(placeholderTransactions);
-  const [auditLogs, setAuditLogs] = useState([]);
+  // const [users, setUsers] = useState(placeholderUsers);
+  const [resources, setResources] = useState([]);
+  // const [transactions, setTransactions] = useState(placeholderTransactions);
+  const [transactions, setTransactions] = useState();
+  const [auditLogs, setAuditLogs] = useState();
   const [analytics, setAnalytics] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [user, setUser] = useState();
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      // Fetching all necessary data
-      const resourcesResponse = await getResources();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        // Fetching all necessary data
+        const resourcesResponse = await getResources();
 
-      // Setting state with fetched data
-      setResources(resourcesResponse.data);
-    } catch (err) {
-      setError(err.message || 'An error occurred while fetching data.');
-    } finally {
-      setLoading(false);
-    }
-  };
+        // Setting state with fetched data
+        setResources(resourcesResponse.data);
+      } catch (err) {
+        setError(err.message || 'An error occurred while fetching resource data.');
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  fetchData();
-}, []);
+    const fetchUsers = async () => {
+      try {
+        setLoading(true);
 
+        const usersResponse = await getUsers();
+
+        setUser(usersResponse.data);
+      } catch (err) {
+        setError(err.message || 'An error occurred while fetching user data.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const fetchTransactions = async () => {
+      try {
+        setLoading(true);
+
+        const transactionsResponse = await getRecentTransactions();
+
+        setTransactions(transactionsResponse.data);
+      } catch (err) {
+        setError(err.message || 'An error occurred while fetching transaction data.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const fetchAuditLogs = async () => {
+      try {
+        setLoading(true);
+
+        const auditLogsResponse = await getAuditLogs();
+
+        setAuditLogs(auditLogsResponse.data);
+      } catch (err) {
+        setError(err.message || 'An error occurred while fetching audit log data.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const fetchAnalytics = async () => {
+      try {
+        setLoading(true);
+
+        const analyticsResponse = await getAnalytics();
+
+        setAnalytics(analyticsResponse.data);
+      } catch (err) {
+        setError(err.message || 'An error occurred while fetching analytics data.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+    fetchAuditLogs();
+    fetchTransactions();
+    fetchUsers();
+    fetchAnalytics();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -246,12 +367,12 @@ useEffect(() => {
             </tr>
           </thead>
           <tbody>
-            {users.map(user => (
-              <tr key={user.id}>
-                <td>{user.userId}</td>
-                <td>{user.userName}</td>
-                <td>{user.role}</td>
-                <td>{user.address}</td>
+            {user?.map((userItem, index) => (
+              <tr key={index}>
+                <td>{userItem.userId}</td>
+                <td>{userItem.userName}</td>
+                <td>{userItem.role}</td>
+                <td>{userItem.address}</td>
                 <td>
                   <button onClick={() => alert('Edit user')}>Edit</button>
                   <button onClick={() => alert('Remove user')}>Remove</button>
@@ -300,7 +421,7 @@ useEffect(() => {
             </tr>
           </thead>
           <tbody>
-            {transactions.map(transaction => (
+            {transactions?.map(transaction => (
               <tr key={transaction.id}>
                 <td>{transaction.id}</td>
                 <td>{transaction.materials.map(m => m.foodType).join(', ')}</td>
@@ -362,7 +483,7 @@ useEffect(() => {
             </tr>
           </thead>
           <tbody>
-            {auditLogsData.map(log => (
+            {auditLogs?.map(log => (
               <tr key={log.id}>
                 <td>{log.id}</td>
                 <td>{log.timestamp}</td>
